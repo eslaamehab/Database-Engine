352e3b329bddd1a2df60553ffe319fbf9a2d62ea
eslaamehab <eslaam.ehaab@gmail.com>
2024-02-26 18:04:25 +0200

    Create GeneralReference.java

---
+++ src/GeneralReference.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.sql.Ref;
+import java.util.ArrayList;
+
+public abstract class GeneralReference {
+
+    public abstract boolean isOverflow();
+    public abstract boolean isRecord();
+    public abstract void updateRef(String oldPage, String newPage) throws DBAppException;
+
+    public ArrayList<Ref> getALLRef() throws DBAppException
+    {
+        ArrayList<Ref> results = new ArrayList<Ref>();
+        if(this instanceof Ref)
+        {
+            results.add((Ref)this);
+        }
+        else
+        {
+            OverflowReference ov = (OverflowReference) this;
+            results.addAll(ov.ALLgetRef());
+        }
+
+        return results;
+
+    }
+
+}
