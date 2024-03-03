976a9ec6314fc70b4e1a5b8ecce9939339a4d1a6
eslaamehab <eslaam.ehaab@gmail.com>
2024-03-03 20:14:03 +0200

    Update LeafNode.java

--- src/LeafNode.java
+++ src/LeafNode.java
@@ -1,4 +1,12 @@
 package src;
 
-public interface LeafNode {
+public interface LeafNode<T extends Comparable<T>> {
+
+    public LeafNode getNext() throws DBAppException;
+    public int getNumberOfKeys();
+    public Comparable<T> getKey(int index);
+    public GeneralReference getRecord(int index);
+
+    public LeafNode<T> searchForUpdateRef(T key);
+    public void updateRef(String oldPage,String newPage,T key) throws DBAppException;
 }
