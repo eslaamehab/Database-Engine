b521879943c98580e2b14984202fa78e5b635085
eslaamehab <eslaam.ehaab@gmail.com>
2024-03-25 13:16:32 +0200

    Create DBAppException.java

---
+++ src/DBGeneralEngine/DBAppException.java
@@ -0,0 +1,11 @@
+package src.DBGeneralEngine;
+
+public class DBAppException extends Exception {
+
+    public DBAppException() {
+    }
+    public DBAppException(String arg0) {
+        super(arg0);
+    }
+
+}
