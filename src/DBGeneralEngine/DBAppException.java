244c9388caca37c4eea4e02eab74c68ff62bc5de
eslaamehab <eslaam.ehaab@gmail.com>
2024-04-10 17:59:31 +0200

    Update DBAppException.java

--- src/DBGeneralEngine/DBAppException.java
+++ src/DBGeneralEngine/DBAppException.java
@@ -3,6 +3,7 @@ package src.DBGeneralEngine;
 public class DBAppException extends Exception {
 
     public DBAppException() {
+        super();
     }
     public DBAppException(String arg0) {
         super(arg0);
