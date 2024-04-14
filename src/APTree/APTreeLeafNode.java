507e4dfcad51bb5395a138ce39d6bbe8471472e5
eslaamehab <eslaam.ehaab@gmail.com>
2024-04-14 18:38:03 +0200

    Refactor APTreeLeadNode.java

---
+++ src/APTree/APTreeLeafNode.java
@@ -0,0 +1,17 @@
+package src.APTree;
+
+import src.DBGeneralEngine.DBAppException;
+import src.Ref.GeneralRef;
+
+public interface APTreeLeafNode<T extends Comparable<T>> {
+
+    APTreeLeafNode getNext() throws DBAppException;
+    int getNumberOfKeys();
+    Comparable<T> getKey(int index);
+    GeneralRef getRecord(int index);
+
+
+    // safe delete below couple methods?
+    APTreeLeafNode<T> searchForUpdateRef(T key);
+    void updateRef(String oldPage, String newPage, T key) throws DBAppException;
+}
