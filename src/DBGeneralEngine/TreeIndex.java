0fac9668bf74105ec6b04f904c522870d76f7415
eslaamehab <eslaam.ehaab@gmail.com>
2024-04-14 18:38:42 +0200

    update TreeIndex.java

--- src/DBGeneralEngine/TreeIndex.java
+++ src/DBGeneralEngine/TreeIndex.java
@@ -1,6 +1,6 @@
-package src.APTree;
+package src.DBGeneralEngine;
 
-import src.DBGeneralEngine.DBAppException;
+import src.APTree.APTreeLeafNode;
 import src.Ref.GeneralRef;
 import src.Ref.Ref;
 
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 
 public interface TreeIndex<T extends Comparable> {
     public GeneralRef search(T key) throws DBAppException;
-    public Ref searchForInsertion(T key, int tableLength) throws DBAppException ;
+    public Ref searchForInsertion(T key, int tableLength) throws DBAppException;
     public boolean delete(T key) throws DBAppException;
     public boolean delete(T key, String PageName) throws DBAppException;
     public void insert(T key, Ref recordReference) throws DBAppException;
-    public LeafNode getLeftmostLeaf() throws DBAppException ;
+    public APTreeLeafNode getLeftmostLeaf() throws DBAppException;
     public ArrayList<GeneralRef> searchMTE(T key) throws DBAppException;
     public ArrayList<GeneralRef> searchMT(T key) throws DBAppException;
-    public void updateRef(String oldPage,String newPage,T key) throws DBAppException;
+    public void updateRef(String oldPage, String newPage, T key) throws DBAppException;
 }
