12effce655952ae186ce27f52993dc4186ab58b1
eslaamehab <eslaam.ehaab@gmail.com>
2024-05-27 08:59:13 +0300

    Add BPTreeTest.java

--- src/BPTree/BPTreeTest.java
+++ src/BPTree/BPTreeTest.java
@@ -1,4 +1,15 @@
 package src.BPTree;
 
+import src.DBGeneralEngine.DBAppException;
+
 public class BPTreeTest {
+
+    public static void main(String[] args) throws DBAppException {
+
+        BPTree<Integer> bpTree = new BPTree<Integer>(4);
+
+        // initialize and manipulate here
+
+    }
+
 }
