78919960187416c17ff09a0ea548919c41c965be
eslaamehab <eslaam.ehaab@gmail.com>
2024-05-27 08:59:42 +0300

    Create CustomPolygon.java

--- src/DBGeneralEngine/CustomPolygon.java
+++ src/DBGeneralEngine/CustomPolygon.java
@@ -5,51 +5,57 @@ import java.io.Serializable;
 
 public class CustomPolygon implements Comparable<CustomPolygon>, Serializable {
 
-        /*      Attributes    */
-public Polygon polygon;
+    /**
+     * Attributes
+     */
+    public Polygon polygon;
 
 
-        /*      Constructor     */
-public CustomPolygon(Polygon polygon){
-        this.polygon=polygon;
-        }
+    /**
+     * Constructor
+     */
+    public CustomPolygon(Polygon polygon) {
+        this.polygon = polygon;
+    }
 
 
-        /*      Methods     */
-@Override
-public int compareTo(CustomPolygon customPolygon){
+    /**
+     * Methods
+     */
+    @Override
+    public int compareTo(CustomPolygon customPolygon) {
         Dimension dimension = polygon.getBounds().getSize();
         int polygonArea = dimension.width * dimension.height;
 
         dimension = customPolygon.polygon.getBounds().getSize();
         int customPolygonArea = dimension.width * dimension.height;
 
-    return polygonArea - customPolygonArea;
-        }
+        return polygonArea - customPolygonArea;
+    }
 
-@Override
-public boolean equals(Object obj){
+    @Override
+    public boolean equals(Object obj) {
 
         CustomPolygon customPolygon = (CustomPolygon) obj;
         return this.compareTo(customPolygon) == 0;
-        }
+    }
 
-@Override
-public String toString(){
+    @Override
+    public String toString() {
 
         StringBuilder stringBuilder = new StringBuilder("Points: ");
         int[] x = polygon.xpoints;
         int[] y = polygon.ypoints;
 
-        for(int i=0; i<y.length; i++){
+        for (int i = 0; i < y.length; i++) {
 
-        stringBuilder.append("(").append(x[i]).append(", ").append(y[i]).append(") ");
+            stringBuilder.append("(").append(x[i]).append(", ").append(y[i]).append(") ");
         }
         Dimension dimension = polygon.getBounds().getSize();
         int polygonArea = dimension.width * dimension.height;
         stringBuilder.append("Area: ").append(polygonArea);
 
         return stringBuilder.toString();
-        }
+    }
 
 }
