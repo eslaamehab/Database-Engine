8a65a10b2da9c115db3d519b8153a183de0a9d47
eslaamehab <eslaam.ehaab@gmail.com>
2024-05-30 01:49:11 +0300

    Add LeafNode Interface

--- src/DBGeneralEngine/LeafNode.java
+++ src/DBGeneralEngine/LeafNode.java
@@ -1,6 +1,5 @@
 package src.DBGeneralEngine;
 
-import src.DBGeneralEngine.DBAppException;
 import src.Ref.GeneralRef;
 
 public interface LeafNode<T extends Comparable<T>> {
@@ -9,6 +8,5 @@ public interface LeafNode<T extends Comparable<T>> {
     int getNumberOfKeys();
     Comparable<T> getKey(int index);
     GeneralRef getRecord(int index);
-
     void updateRef(String oldPage, String newPage, T key) throws DBAppException;
 }
