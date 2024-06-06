f66fc9450f27811b6bff6ca3403ed498eb34e08c
eslaamehab <eslaam.ehaab@gmail.com>
2024-06-06 08:29:09 +0300

    Add TreeIndex.java Interface

--- src/DBGeneralEngine/TreeIndex.java
+++ src/DBGeneralEngine/TreeIndex.java
@@ -6,13 +6,23 @@ import src.Ref.Ref;
 import java.util.ArrayList;
 
 public interface TreeIndex<T extends Comparable> {
-    public GeneralRef search(T key) throws DBAppException;
-    public Ref searchForInsertion(T key, int tableLength) throws DBAppException;
-    public boolean delete(T key) throws DBAppException;
-    public boolean delete(T key, String PageName) throws DBAppException;
-    public void insert(T key, Ref recordReference) throws DBAppException;
-    public LeafNode getLeftmostLeaf() throws DBAppException;
-    public ArrayList<GeneralRef> searchMTE(T key) throws DBAppException;
-    public ArrayList<GeneralRef> searchMT(T key) throws DBAppException;
-    public void updateRef(String oldPage, String newPage, T key) throws DBAppException;
+
+    boolean delete(T key) throws DBAppException;
+
+    boolean delete(T key, String PageName) throws DBAppException;
+
+    void insert(T key, Ref recordReference) throws DBAppException;
+
+    void updateRef(String oldPage, String newPage, T key) throws DBAppException;
+
+    GeneralRef search(T key) throws DBAppException;
+
+    Ref searchForInsertion(T key, int tableLength) throws DBAppException;
+
+    LeafNode getLeftmostLeaf() throws DBAppException;
+
+    ArrayList<GeneralRef> searchMTE(T key) throws DBAppException;
+
+    ArrayList<GeneralRef> searchMT(T key) throws DBAppException;
+
 }
