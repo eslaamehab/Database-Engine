120125c9c7cd01096f5559c9b49b5223314dfa25
eslaamehab <eslaam.ehaab@gmail.com>
2024-06-09 02:35:19 +0300

    Add Tuple.java

--- src/DBGeneralEngine/Tuple.java
+++ src/DBGeneralEngine/Tuple.java
@@ -5,23 +5,39 @@ import java.util.Vector;
 
 public class Tuple implements Serializable {
 
-    /*      Attributes     */
-    private Vector attributes = new Vector();
+
+    /**
+     * Attributes
+     */
+    private Vector attributes;
 
 
-    /*      Getters & Setters     */
+    /**
+     * Constructor
+     */
+    public Tuple(Vector attributes) {
+        this.attributes = attributes;
+    }
+
+
+    /**
+     * Getters & Setters
+     */
     public Vector getAttributes() {
         return attributes;
     }
+
     public void setAttributes(Vector attributes) {
         this.attributes = attributes;
     }
 
 
-    /*      Methods     */
+    /**
+     * Rest of the functions
+     */
     public boolean equals(Object obj) {
-        Tuple x=(Tuple)obj;
-        Tuple y= this;
+        Tuple x = (Tuple) obj;
+        Tuple y = this;
         Object compare1 = x.attributes.get(attributes.size() - 1);
         Object compare2 = y.attributes.get(attributes.size() - 1);
 
@@ -29,7 +45,7 @@ public class Tuple implements Serializable {
     }
 
     public int hashCode() {
-        return (int)attributes.get(attributes.size()-1);
+        return (int) attributes.get(attributes.size() - 1);
     }
 
     public void addAttribute(Object obj) {
@@ -39,7 +55,7 @@ public class Tuple implements Serializable {
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int i=0;i<attributes.size()-1;i++) {
+        for (int i = 0; i < attributes.size() - 1; i++) {
             Object x = attributes.get(i);
             stringBuilder.append((x != null) ? (x + "\t") : ("null\n"));
         }
