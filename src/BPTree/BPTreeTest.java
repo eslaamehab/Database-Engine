fdcd17f9a0f4d9a6199f9eaa0cec1efca489a88e
eslaamehab <eslaam.ehaab@gmail.com>
2024-06-24 18:24:26 +0300

    Add BPTreeTest.java

--- src/BPTree/BPTreeTest.java
+++ src/BPTree/BPTreeTest.java
@@ -2,13 +2,17 @@ package src.BPTree;
 
 import src.DBGeneralEngine.DBAppException;
 
+/**
+ * This class is used to test the functionality of the BPTree data structure.
+ * It creates an instance of the BPTree in order to initialize and manipulate the tree.
+ */
 public class BPTreeTest {
 
     public static void main(String[] args) throws DBAppException {
 
         BPTree<Integer> bpTree = new BPTree<Integer>(4);
 
-        // initialize and manipulate here
+        // test and manipulate here
 
     }
 
