87d65f455f4c9308aa2cdc008c1c5c718f78bbbc
eslaamehab <eslaam.ehaab@gmail.com>
2024-06-25 13:37:08 +0300

    Final Push PushUpTree.java

--- src/BPTree/PushUpBPTree.java
+++ src/BPTree/PushUpBPTree.java
@@ -1,15 +1,19 @@
 package src.BPTree;
 
+
 /**
- * This class is used for push keys up to the inner nodes in case
- * of splitting at a lower level
+ * This class is used for pushing keys up to the inner nodes in case of splitting at a lower level in a B+ Tree data structure.
+ * <p>
+ * It is used to encapsulate/insert a new node into the tree and updating the parent nodes accordingly.
  */
 public class PushUpBPTree<T extends Comparable<T>> {
 
-
-
     /**
      * Attributes
+     * <p>
+     * <p>
+     * newNode  -> the new node to be inserted into the tree.
+     * key      -> the key value associated with the new node.
      */
     BPTreeNode<T> newNode;
     Comparable<T> key;
@@ -17,9 +21,12 @@ public class PushUpBPTree<T extends Comparable<T>> {
 
     /**
      * Constructor
+     * Initializes the PushUpBPTree object with the given newNode and key.
+     *
+     * @param newNode the new node to be inserted into the tree
+     * @param key     the key value associated with the new node
      */
-    public PushUpBPTree(BPTreeNode<T> newNode, Comparable<T> key)
-    {
+    public PushUpBPTree(BPTreeNode<T> newNode, Comparable<T> key) {
         this.newNode = newNode;
         this.key = key;
     }
