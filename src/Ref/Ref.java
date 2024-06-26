e319370f9d418811e412aeec32106db479507a37
eslaamehab <eslaam.ehaab@gmail.com>
2024-06-26 12:07:20 +0300

    Add class comment Ref.java

--- src/Ref/Ref.java
+++ src/Ref/Ref.java
@@ -2,14 +2,35 @@ package src.Ref;
 
 import java.io.Serializable;
 
+
+
+/**
+ * This class represents a reference object that contains a page number.
+ * It inherits the basic functionality of a general reference object, such as the ability to store and retrieve a value.
+ * It implements 'Serializable' interface to allow instances of `OverflowRef` to be easily serialized and deserialized
+ * In order to enable persistence and transfer of the object's state.
+ *
+ * Additional notes:-
+ * This class overrides the `equals` and `hashCode` methods to provide custom implementations for comparing and hashing `Ref` objects based on their page number.
+ * This class also overrides the `isOverflow` method from the `GeneralRef` class, indicating that this type of reference does not have any overflow conditions.
+ */
 public class Ref extends GeneralRef implements Serializable
 {
 
     /**
      * Attributes
+     * <p>
+     * pageNo   -> A String representing the page number associated with this reference
      */
     private String pageNo;
 
+
+    /**
+     * Constructor
+     * Creates a reference object by initializing the `pageNo` attribute with the provided page number.
+     *
+     * @param pageNo the page number to be associated with this reference
+     */
     public Ref(String pageNo)
     {
         this.pageNo = pageNo;
@@ -37,7 +58,13 @@ public class Ref extends GeneralRef implements Serializable
     }
 
 
-
+    /**
+     * Override of the `equals` method
+     * Compares the page numbers of two `Ref` objects for equality
+     *
+     * @param obj the object to compare with
+     * @return `true` if the page numbers are equal or `false` otherwise
+     */
     public boolean equals(Object obj) {
         Ref x= (Ref) obj;
         String firstNo = x.pageNo;
@@ -46,6 +73,12 @@ public class Ref extends GeneralRef implements Serializable
         return firstNo.equals(secondNo);
     }
 
+    /**
+     * Override of the `hashCode` method
+     * Generates a hash code for a `Ref` object based on its page number
+     *
+     * @return the hash code for this `Ref` object
+     */
     public int hashCode() {
         char[] charArray = pageNo.toCharArray();
         StringBuilder stringBuilder = new StringBuilder();
@@ -58,10 +91,22 @@ public class Ref extends GeneralRef implements Serializable
         return Integer.parseInt(stringBuilder.toString());
     }
 
+    /**
+     * Updates the page number associated with this reference
+     *
+     * @param oldPage the old page number
+     * @param newPage the new page number
+     */
     public void updateRef(String oldPage, String newPage) {
         pageNo = newPage;
     }
 
+    /**
+     * Override of the `isOverflow` method from the `GeneralRef` class
+     * Indicates that this type of reference does not have any overflow conditions
+     *
+     * @return `false` to indicate no overflow condition
+     */
     @Override
     public boolean isOverflow() {
         return false;
