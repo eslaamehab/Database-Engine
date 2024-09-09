191af7fd3208a013e0f312ff698d0fb78f5e6298
eslaamehab <eslaam.ehaab@gmail.com>
2024-09-09 22:05:05 +0300

    finalPush CustomPolygon.java

--- src/DBGeneralEngine/CustomPolygon.java
+++ src/DBGeneralEngine/CustomPolygon.java
@@ -3,16 +3,26 @@ package src.DBGeneralEngine;
 import java.awt.*;
 import java.io.Serializable;
 
+
+/**
+ * CustomPolygon class represents a polygon with additional functionalities.
+ * This class implements Comparable to allow sorting based on the polygon's area.
+ */
 public class CustomPolygon implements Comparable<CustomPolygon>, Serializable {
 
     /**
      * Attributes
+     *
+     * polygon -> The underlying Polygon object representing the shape.
      */
     public Polygon polygon;
 
 
     /**
      * Constructor
+     * Initializes a CustomPolygon instance with the specified Polygon.
+     *
+     * @param polygon the Polygon to be encapsulated in this CustomPolygon
      */
     public CustomPolygon(Polygon polygon) {
         this.polygon = polygon;
@@ -20,7 +30,12 @@ public class CustomPolygon implements Comparable<CustomPolygon>, Serializable {
 
 
     /**
-     * Methods
+     * Compares this CustomPolygon with another CustomPolygon based on their areas.
+     *
+     * @param customPolygon the CustomPolygon to compare against
+     *
+     * @return a negative integer, zero, or a positive integer.
+     * When this polygon is less than, equal to, or greater than the specified polygon
      */
     @Override
     public int compareTo(CustomPolygon customPolygon) {
@@ -33,6 +48,14 @@ public class CustomPolygon implements Comparable<CustomPolygon>, Serializable {
         return polygonArea - customPolygonArea;
     }
 
+
+    /**
+     * Checks if this CustomPolygon is equal to another object.
+     * Two CustomPolygons are considered equal if they have the same area.
+     *
+     * @param obj the object to compare with
+     * @return true if the polygons are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
 
@@ -40,6 +63,13 @@ public class CustomPolygon implements Comparable<CustomPolygon>, Serializable {
         return this.compareTo(customPolygon) == 0;
     }
 
+
+    /**
+     * Returns a string representation of the CustomPolygon.
+     * This includes the points of the polygon and its area.
+     *
+     * @return a string describing the points and area of the polygon
+     */
     @Override
     public String toString() {
 
@@ -59,3 +89,5 @@ public class CustomPolygon implements Comparable<CustomPolygon>, Serializable {
     }
 
 }
+
+
