6d1e51ca7b78793bf4f153f7494261acddc009b6
eslaamehab <eslaam.ehaab@gmail.com>
2024-09-10 20:46:46 +0300

    Add DBAppTest.java

--- src/DBGeneralEngine/DBAppTest.java
+++ src/DBGeneralEngine/DBAppTest.java
@@ -5,10 +5,20 @@ import java.util.Vector;
 
 import static java.util.Objects.hash;
 
+
+/**
+ * DBAppTest class representing a test implementation for a database application.
+ * This class manages a collection of inserts associated with a specific table.
+ */
 public class DBAppTest implements Serializable {
 
     /**
      * Attributes
+     *
+     * inserts -> Stores the inserted objects for the table
+     * nextID -> Static variable to keep track of the next available ID
+     * id -> Unique identifier for this instance
+     * tableName -> Name of the table associated with this instance
      */
     private final Vector<Object> inserts;
     private static long nextId = 0L;
@@ -18,6 +28,10 @@ public class DBAppTest implements Serializable {
 
     /**
      * Constructor
+     * Initializes the DBAppTest with a specified table name and prepares to manage inserts.
+     *
+     * @param tableName the name of the table for this instance
+     * @throws IOException if an error occurs while initializing the next ID
      */
     public DBAppTest(String tableName) throws IOException {
         this.tableName = tableName;
@@ -30,19 +44,42 @@ public class DBAppTest implements Serializable {
 
     /**
      * Getters & Setters
+
+
+     * Returns the Vector of inserted objects.
+     *
+     * @return a Vector containing the inserted objects
      */
     public Vector<Object> getInserts() {
         return inserts;
     }
 
+
+    /**
+     * Sets the next available ID for instances of DBAppTest.
+     *
+     * @param nextId the new next ID to be set
+     */
     public static void setNextId(long nextId) {
         DBAppTest.nextId = nextId;
     }
 
+
+    /**
+     * Returns the name of the table associated with this instance.
+     *
+     * @return the name of the table
+     */
     public String getTableName() {
         return tableName;
     }
 
+
+    /**
+     * Retrieves the next available ID from storage.
+     *
+     * @return the next available ID
+     */
     public long getNextId() {
         long id = 0;
         try {
@@ -68,17 +105,32 @@ public class DBAppTest implements Serializable {
         return id;
     }
 
+
+    /**
+     * Returns the unique identifier for this instance.
+     *
+     * @return the ID of this instance
+     */
     public long getId() {
         return this.id;
     }
 
+
+    /**
+     * Retrieves the object at the specified index in the inserts Vector.
+     *
+     * @param id the index of the object to retrieve
+     * @return the object at the specified index
+     */
     public Object get(int id) {
         return inserts.get(id);
     }
 
 
     /**
-     * Methods
+     * Saves the next available ID to storage.
+     *
+     * @throws IOException if an error occurs while saving the ID
      */
     public void saveNextId() throws IOException {
 
@@ -93,28 +145,66 @@ public class DBAppTest implements Serializable {
     }
 
 
+    /**
+     * Checks if the specified ID matches this instance's ID.
+     *
+     * @param id the ID to check against
+     * @return true if the IDs match, false otherwise
+     */
     public boolean hasId(long id) {
         return this.id == id;
     }
 
 
+    /**
+     * Adds an object at the specified index in the inserts Vector.
+     *
+     * @param id  the index at which to insert the object
+     * @param obj the object to be added
+     */
     public void add(int id, Object obj) {
         inserts.insertElementAt(obj, id);
 
     }
 
+
+    /**
+     * Updates the object at the specified index in the inserts Vector.
+     *
+     * @param id  the index of the object to update
+     * @param obj the new object to replace the existing one
+     */
     public void update(int id, Object obj) {
         inserts.setElementAt(obj, id);
     }
 
+
+    /**
+     * Retrieves all values from the inserts Vector.
+     *
+     * @return a Vector containing all inserted objects
+     */
     public Vector<Object> getValues() {
         return inserts;
     }
 
+
+    /**
+     * Adds an object to the end of the inserts Vector.
+     *
+     * @param obj the object to be added
+     */
     public void add(Object obj) {
         inserts.add(obj);
     }
 
+
+    /**
+     * Returns a string representation of the DBAppTest instance.
+     * This includes all inserted objects and the instance's ID.
+     *
+     * @return a string representation of the instance
+     */
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         for (Object obj : inserts)
@@ -123,6 +213,12 @@ public class DBAppTest implements Serializable {
         return stringBuilder.toString();
     }
 
+
+    /**
+     * Computes a hash code for this instance based on its attributes.
+     *
+     * @return the hash code for this instance
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -131,6 +227,13 @@ public class DBAppTest implements Serializable {
         return result;
     }
 
+
+    /**
+     * Checks equality between this instance and another object.
+     *
+     * @param obj the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -141,9 +244,13 @@ public class DBAppTest implements Serializable {
         return getId() == testAPTree.getId() && inserts.equals(testAPTree.inserts);
     }
 
+
+    /**
+     * Main method for testing the DBAppTest class.
+     * Dummy entries are instantiated here for testing purposes.
+     */
     public static void main(String[] args) throws IOException {
 
-        // instantiate dummmy entries here
 
     }
 }
